dd147bd527e2b5f4b32ade2d9f3957c0a3be80b9
spoofhopper <morgan.holland@gmail.com>
2015-09-10 16:31:13 -0700

    finished blog and moved over bookazon

---
+++ MorganHolland-spoofhopper/hw_12_bookazon/bookazon_app/app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+
+  #READ: this will retrieve all books thru active_record
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = get_book
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @book = get_book
+  end
+
+  def update
+    @book = get_book
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = find_book
+    @book.destroy
+    redirect_to books_path
+  end
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :year_published, :author, :available, :genre, :image_url)
+
+  end
+
+
+  def get_book
+    Book.find(params[:id])
+  end
+
+
+end
