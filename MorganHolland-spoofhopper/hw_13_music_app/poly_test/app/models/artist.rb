c8c63a7c3e86f9fb791100c474833f657f2157f5
Morgan Holland <morgan.holland@gmail.com>
2015-09-21 11:13:36 -0700

    fixed song.artist reference

---
+++ MorganHolland-spoofhopper/hw_13_music_app/poly_test/app/models/artist.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  image_url   :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Artist < ActiveRecord::Base
+end
---
+++ MorganHolland-spoofhopper/hw_13_music_app/poly_test/app/models/song.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  length     :string
+#  genre      :string
+#  year       :integer
+#  artist_id  :integer
+#  lyrics     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Song < ActiveRecord::Base
+  belongs_to :artist
+end
